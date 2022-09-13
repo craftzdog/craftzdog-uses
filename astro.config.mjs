b81a726131885125307457e493293d8c5ca9c5e4
Takuya Matsuyama <hi@craftz.dog>
2022-09-14 08:35:03 +0900

    fix(meta): update website url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://uses.craftz.dog/',
   integrations: [sitemap(), react(), image(), tailwind()]
 })
