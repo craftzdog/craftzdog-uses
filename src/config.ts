71e970b4eb84549bfc21ff413abfb3037b5e4079
Takuya Matsuyama <hi@craftz.dog>
2022-09-14 08:34:40 +0900

    fix(meta): update website info

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'What I use - Takuya Matsuyama'
+export const SITE_DESCRIPTION = 'A curated list of the tech I use'
+export const HOMEPAGE_URL = 'https://www.craftz.dog/'
