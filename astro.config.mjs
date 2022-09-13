726c53a45c02ddfe10d7d304211498895f92f4ed
Takuya Matsuyama <hi@craftz.dog>
2022-09-14 08:09:42 +0900

    chore(dep): add react and image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-import { defineConfig } from 'astro/config';
-
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config'
+import sitemap from '@astrojs/sitemap'
+import react from '@astrojs/react'
+import image from '@astrojs/image'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [sitemap()],
-});
+  integrations: [sitemap(), react(), image()]
+})
