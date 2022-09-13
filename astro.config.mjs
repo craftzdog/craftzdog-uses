50ef27a89590459aa9e3c971ca82954128ba2244
Takuya Matsuyama <hi@craftz.dog>
2022-09-14 08:21:06 +0900

    chore(dep): add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@ import { defineConfig } from 'astro/config'
 import sitemap from '@astrojs/sitemap'
 import react from '@astrojs/react'
 import image from '@astrojs/image'
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [sitemap(), react(), image()]
+  integrations: [sitemap(), react(), image(), tailwind()]
 })
