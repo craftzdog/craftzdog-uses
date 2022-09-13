09c9c10fc1f4fc91187ef4b9c5febf7f2412a945
Takuya Matsuyama <hi@craftz.dog>
2022-09-14 07:56:31 +0900

    chore(dep): remove mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://example.com',
+  integrations: [sitemap()],
 });
