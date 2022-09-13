8f7b18525e788871701673c93346c89c3223c4ba
Takuya Matsuyama <hi@craftz.dog>
2022-09-14 08:24:17 +0900

    feat(css): add font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        mplus: ["'M PLUS Rounded 1c'", 'Verdana', 'sans-serif']
+      }
+    }
+  },
+  plugins: []
 }
