249b17da4a249bf39a659cc895dc1bea13e2d2d9
Takuya Matsuyama <hi@craftz.dog>
2022-09-14 10:17:13 +0900

    feat(posts): style posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,29 @@ import sitemap from '@astrojs/sitemap'
 import react from '@astrojs/react'
 import image from '@astrojs/image'
 import tailwind from '@astrojs/tailwind'
+import addClasses from 'rehype-add-classes'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://uses.craftz.dog/',
-  integrations: [sitemap(), react(), image(), tailwind()]
+  integrations: [sitemap(), react(), image(), tailwind()],
+  markdown: {
+    extendDefaultPlugins: true,
+    rehypePlugins: [
+      [
+        addClasses,
+        {
+          h1: 'text-4xl font-bold font-mplus',
+          h2: 'text-2xl font-bold font-mplus',
+          h3: 'text-xl font-bold font-mplus',
+          h4: 'text-lg font-bold font-mplus',
+          h5: 'font-bold font-mplus',
+          h6: 'font-bold font-mplus',
+          img: 'border border-slate-300 dark:border-zinc-700 rounded-xl mb-6',
+          p: 'mb-6',
+          a: 'underline underline-offset-2 hover:text-orange-500 decoration-orange-500'
+        }
+      ]
+    ]
+  }
 })
