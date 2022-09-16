57e7e8af93c30fd4b94160842829a80175d72b87
Takuya Matsuyama <hi@craftz.dog>
2022-09-16 10:43:29 +0900

    fix(theme): incorrect media selector

--- src/components/ThemeToggleButton.tsx
+++ src/components/ThemeToggleButton.tsx
@@ -12,7 +12,7 @@ export default function ThemeToggle() {
     if (typeof localStorage !== 'undefined' && localStorage.getItem('theme')) {
       return localStorage.getItem('theme')
     }
-    if (window.matchMedia('(prefers-color-schheme: dark)').matches) {
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
       return 'dark'
     }
     return 'light'
