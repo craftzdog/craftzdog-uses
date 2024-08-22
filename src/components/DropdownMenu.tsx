f4bf359e8a67186dfd74ecf1e1bca1341f697026
Takuya Matsuyama <hi@craftz.dog>
2024-08-22 10:15:50 +0900

    feat(deps): bump up heaadless-ui and astro and other tools

--- src/components/DropdownMenu.tsx
+++ src/components/DropdownMenu.tsx
@@ -1,4 +1,4 @@
-import { Menu, Transition } from '@headlessui/react'
+import { Menu, MenuItems, MenuButton, Transition } from '@headlessui/react'
 import React, { Fragment } from 'react'
 import { IoMenu } from 'react-icons/io5'
 import DropdownMenuItem from './DropdownMenuItem'
@@ -11,12 +11,12 @@ export default function DropdownMenu({ tags }: Props) {
   return (
     <Menu as="div" className="relative inline-block text-left">
       <div>
-        <Menu.Button
+        <MenuButton
           className="inline-flex justify-center rounded-md border border-zinc-400 dark:border-zinc-700 px-2 py-2 text-sm font-medium shadow-sm hover:bg-orange-200 dark:hover:bg-zinc-800 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100 transition-all"
           aria-label="menu"
         >
           <IoMenu className="h-5 w-5" />
-        </Menu.Button>
+        </MenuButton>
       </div>
 
       <Transition
@@ -28,7 +28,7 @@ export default function DropdownMenu({ tags }: Props) {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md border border-zinc-400 dark:border-zinc-700 bg-orange-50 dark:bg-zinc-800 shadow-xl ring-1 ring-black ring-opacity-5 focus:outline-none divide-zinc-400 dark:divide-zinc-700">
+        <MenuItems className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md border border-zinc-400 dark:border-zinc-700 bg-orange-50 dark:bg-zinc-800 shadow-xl ring-1 ring-black ring-opacity-5 focus:outline-none divide-zinc-400 dark:divide-zinc-700">
           <div className="py-1">
             <div className="px-3 py-2 uppercase font-bold text-xs">
               Categories
@@ -44,7 +44,7 @@ export default function DropdownMenu({ tags }: Props) {
               )
             })}
           </div>
-        </Menu.Items>
+        </MenuItems>
       </Transition>
     </Menu>
   )
